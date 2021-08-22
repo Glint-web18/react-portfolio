41c51d98e386ecb8b90ead793dc00b1a4fd082f6
Justin <jusglinton@gmail.com>
2021-08-21 21:45:56 -0400

    portfolio

--- portfolio/src/components/intro/Intro.jsx
+++ portfolio/src/components/intro/Intro.jsx
@@ -3,7 +3,21 @@ import './intro.scss'
 export default function Intro() {
     return (
         <div className = "intro" id = "intro">
-            
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Owner's Name</h1>
+                    <h3>Freelance <span>Developer</span></h3>
+                </div>
+                <a href="#portfolios">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
         </div>
     )
 }
