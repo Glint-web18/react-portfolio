1232817ff553a6b9c8a19caf0340453ac4a60fe0
Justin <jusglinton@gmail.com>
2021-08-25 22:11:41 -0400

    portfolio

--- portfolio/src/components/portfolios/Portfolios.jsx
+++ portfolio/src/components/portfolios/Portfolios.jsx
@@ -1,47 +1,84 @@
-import "./portfolios.scss"
+import { useEffect, useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
+import "./portfolios.scss";
+import{ featuredPortfolio,
+    webPortfolio,
+    mobilePortfolio,
+    designPortfolio,
+    contentPortfolio
+} from "../../data";
 
 export default function Portfolios() {
+    const[selected, setSelected] = useState("featured")
+    const[data, setData] = useState([])
+
+    const list =[
+        {
+        id: "featured",
+        title:"Featured",
+    },
+    {
+        id: "web",
+        title:"Web App",
+    },
+    {
+        id: "mobile",
+        title: "Mobile App",
+    },
+    {
+        id: "design",
+        title: "Design",
+    },
+    {
+        id: "content",
+        title: "Content",
+    },
+    
+];
+
+    useEffect(()=> {
+
+        switch(selected){
+            case "featured":
+                setData(featuredPortfolio);
+                break
+            case "web":
+                setData( webPortfolio);
+                break
+            case "moblie":
+                setData(mobilePortfolio);
+                break
+            case "design":
+                setData(designPortfolio);
+                break
+            case "content":
+                setData(contentPortfolio);
+                break
+            default:
+                setData(featuredPortfolio);
+        }
+    }, [selected])
+
     return (
         <div className = "portfolios" id = "portfolios">
             <h1>Portfolio</h1>
             <ul>
-                <li className ="active">Featured</li>
-                <li>Web App</li>
-                <li>Mobile App</li>
-                <li>Design</li>
-                <li>Branding</li>
+                {list.map(item => (
+                    <PortfolioList 
+                    title = {item.title} 
+                    active={selected === item.id} 
+                    setSelected= {setSelected}
+                    id = {item.id}/>
+                ))}
             </ul>
             <div className="container">
+                {data.map(d=>(
                 <div className="item">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU" 
+                    <img src={d.img} 
                     alt="" />
-                    <h3>Banking App</h3>
+                    <h3>{d.title}</h3>
                 </div>
-                <div className="item">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU" 
-                    alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU" 
-                    alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU" 
-                    alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU" 
-                    alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU" 
-                    alt="" />
-                    <h3>Banking App</h3>
-                </div>
+                ))}
             </div>
         </div>
     )
