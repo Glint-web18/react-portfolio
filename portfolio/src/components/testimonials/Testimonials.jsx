885f3d7fe8dfdccc3d40744abc47ed898781168a
Justin <jusglinton@gmail.com>
2021-08-26 23:49:34 -0400

    portfolio

--- portfolio/src/components/testimonials/Testimonials.jsx
+++ portfolio/src/components/testimonials/Testimonials.jsx
@@ -3,7 +3,14 @@ import "./testimonials.scss"
 export default function Contacts() {
     return (
         <div className = "testimonials" id = "testimonials"> 
-
+            <h1>Testimonials</h1>
+            <div className="container">
+                <div className="card">
+                    <div className="top"></div>
+                    <div className="center"></div>
+                    <div className="bottom"></div>
+                </div>
+            </div>
             
         </div>
     )
--- portfolio/src/components/works/Works.jsx
+++ portfolio/src/components/works/Works.jsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import "./works.scss"
 
 export default function Works() {
-
+    const [currentSlide, setCurrentSlide] = useState(0)
     const data = [
       { 
       id: "1",
@@ -31,19 +32,26 @@ export default function Works() {
         "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
         },
     ];
+
+    const handleClick = (way)=> {
+        way === "left" ? setCurrentSlide(currentSlide > 0 ? currentSlide -1 : 2) :
+        setCurrentSlide(currentSlide<data.length -1 ? currentSlide + 1 :0);
+    }
+
     return (
         <div className = "works" id = 'works'>
-            <div className="slider">
+            <div className="slider" style = {{ transform: `translateX(-${currentSlide * 100}vw )`}}
+            >
+               {data.map((d) => (
                 <div className="container">
                     <div className="item">
                         <div className="left">
                             <div className="leftContainer">
                                 <div className="imgContainer">
-                                    <img src="assests/mobile.png" alt="" />
+                                    <img src={d.icon} alt="" />
                                 </div>
-                                <h2>Title</h2>
-                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab at aliquam votates
-                                 vgni.</p>
+                                <h2>{d.title}</h2>
+                                <p>{d.desc}</p>
                                  <span>Projects</span>
                             </div>
                         </div>
@@ -52,10 +60,11 @@ export default function Works() {
                              alt="" />
                         </div>
                     </div>
-                </div>
+                </div> 
+                ))}
             </div>
-            <img src="assets/arrow.png" className=" arrow left" alt="" />
-            <img src="assets/arrow.png" className=" arrow right" alt="" />
+            <img src="assets/arrow.png" className=" arrow left" alt="" onClick= {()=>handleClick("left")}/>
+            <img src="assets/arrow.png" className=" arrow right" alt="" onClick= {()=>handleClick()} />
         </div>
     );
 }
